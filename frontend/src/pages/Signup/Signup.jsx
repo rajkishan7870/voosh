cecd7c878d175a28ee7898c9021c5d63f7f73f2f
Kishan Kumar <rajkishan7870@gmail.com>
2024-09-07 01:58:37 +0530

    signup page frontend done

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import style from './Signup.module.css'
+import Navbar from '../../components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
 
 export default function Signup() {
   return (
-    <div>
-      Signup
+    <div >
+      <Navbar />
+      <div className={style.outerDivSignup}>
+        <div className={style.signupParent}>
+          <h2>Signup</h2>
+          <div className={style.signupForm}>
+            <input placeholder="First Name" required />
+            <input placeholder="Last Name" required />
+            <input placeholder="Email" required />
+            <input placeholder="Password" required />
+            <input placeholder="Confirm Password" required />
+            <button>SignUp</button>
+            <div className={style.signupFooter}>
+              <p>Already have an account? <Link className={style.redirectLogin} to= "/login">Login</Link></p>
+              <button className={style.googleBtn}>Signup with Google</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
